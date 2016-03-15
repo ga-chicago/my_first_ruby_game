0eddb586905c342ea29ca075b04e262f53884eea
James Traver <jtraverjr@gmail.com>
2016-03-15 12:15:38 -0500

    yesss ruby

---
+++ escape.rb
@@ -0,0 +1,11 @@
+single_quotes = 'Bill said, \'Wake up\''
+p single_quotes
+# escaping onllllyy
+# onllllyy
+# only
+# work with single quotes
+# sorry, double quotes... you get #{} ... jerk
+
+apa_citation = 'Cole (2006) points out that data from NASA’s Spitzer Telescope has verified the existence of \'hot Jupiters\' and that “measurements confirm that each planet is indeed a hot Jupiter with surface temperatures of the order of 1,000 K” (p370).'
+
+p apa_citation
---
+++ mad_libs.rb
@@ -0,0 +1,14 @@
+
+
+character = ''
+place = ''
+adventure = ''
+goal = ''
+enemy = ''
+victory_speech = ''
+
+# use string interpolate (aka templating) to create a paragraph using these variables
+
+paragraph = "#{character} did something"
+
+p paragraph
